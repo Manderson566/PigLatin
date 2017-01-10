a59d650b1e731624e9225f130f3dc70ccdcd336f
Michael Anderson <Micmail566@gmail.com>
2017-01-09 22:26:54 -0500

    Final. Could not quite figure out sentences.

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -20,9 +20,9 @@ namespace PigLatin
             char firstLetter = WordEntered[0];
             char SecondLetter = WordEntered[1];
             string Firstletter = firstLetter.ToString();
-            string firstTwoletters = WordEntered.Substring(0,1);
+            string firstTwoletters = WordEntered.Substring(0, 2);
             string FirstVowel = firstLetter.ToString();
-            char[] Conletters = { 'B', 'C' ,'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+            char[] Conletters = { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
             string Cons = new string(Conletters);
             char[] Vletters = { 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u' };
             string vowels = new string(Vletters);
@@ -31,18 +31,23 @@ namespace PigLatin
                 Console.WriteLine($"{WordEntered}way");
             }
             else if (Cons.Contains(Firstletter) && Cons.Contains(SecondLetter))
-                {
-                string RemoveFirstTwo = WordEntered.Remove(0 ,2);
+            {
+                string RemoveFirstTwo = WordEntered.Remove(0, 2);
                 Console.WriteLine($"{RemoveFirstTwo}{firstTwoletters}ay");
             }
             else if (Cons.Contains(Firstletter) && vowels.Contains(SecondLetter))
             {
                 string RemoveFirstOne = WordEntered.Remove(0, 1);
                 Console.WriteLine($"{RemoveFirstOne}{Firstletter}ay");
+                Console.WriteLine();
             }
+
         }
+
     }
 }
+
+
 
 
 
